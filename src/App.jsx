d74a9ef2f6e1992f4ebf48ef974d70c1c7fb7dc2
Y.KangHyeok <heavenin24@naver.com>
2025-04-14 10:55:22 +0900

    💄 Update css style
    기본 todo css업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,23 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  const [todo, setTodo] = useState([
+    {
+      id: Number(new Date()),
+      content: "안녕하세요",
+    },
+  ]);
+  return (
+    <>
+      <input />
+      <button>추가</button>
+      <ul>
+        {todo.map((el) => (
+          <li key={el.id}>{el.content}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
